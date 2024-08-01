0560b2284d7879a050edf34f6f62999da44f77fd
YoungTx <tianxiao0907@gmail.com>
2024-08-01 18:11:01 +0800

    启用开发环境下边缘运行时的网关创建功能

--- src/app/api/gateway/route.ts
+++ src/app/api/gateway/route.ts
@@ -7,9 +7,5 @@ export async function OPTIONS(req: NextRequest, res: NextResponse) {
 
 export async function POST(req: Request) {
 	console.log('in gateway', process.env.NODE_ENV)
-	if (process.env.NODE_ENV === 'development') {
-		return createGatewayOnEdgeRuntime()(req)
-	}
-
-	return new Response('gateway')
+	return createGatewayOnEdgeRuntime()(req)
 }
